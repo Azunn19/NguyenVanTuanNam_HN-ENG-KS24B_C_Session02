6efb1f245af824fb015f4a218567f6e666d4c386
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-11-20 16:00:58 +0700

    Upload bài tập session02-Ex06

---
+++ Session02-Ex06.c
@@ -0,0 +1,10 @@
+int main(void){
+	float pi = 3.14;
+	float r = 4;
+	float C = 2*r*pi;
+	float S = r*r*pi;
+	printf("Chu vi hinh tron la: %d\n", C);
+	printf("Dien tich hinh tron la: %d\n", S);
+	
+	return 0;
+}
