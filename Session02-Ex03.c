b9d741290805cdc4abfa6d54ce1f681560745478
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-11-20 16:00:10 +0700

    Upload bài tập session02-Ex03

---
+++ Session02-Ex03.c
@@ -0,0 +1,13 @@
+int main(void) {
+    int a = 10;
+    int b = 2;
+    int c = a + b;
+    int d = a - b;
+    int e = a*b;
+    int f = a/b;
+    printf("Tong cua 2 so là: %d\n", c);
+    printf("Hieu cua 2 so là: %d\n", d);
+    printf("Tich cua 2 so là: %d\n", e);
+    printf("Thuong cua 2 so là: %d\n", f);
+    return 0;
+} 
