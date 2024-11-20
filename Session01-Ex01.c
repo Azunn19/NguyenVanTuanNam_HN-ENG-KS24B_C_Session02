08b95e2559796a96d2686e8a76e9f094abedc08d
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-11-20 15:55:09 +0700

    Upload bài tập session01-Ex01

---
+++ Session01-Ex01.c
@@ -0,0 +1,8 @@
+int a;
+//Luu gia tri la so nguyen voi kich thuoc 4 byte
+float b;
+//Luu gia tri la so thuc voi kich thuoc 4 byte
+double c;
+//Luu gia tri la so thuc voi kich thuoc 8 byte
+char d;
+//Luu gia tri la ky tu voi kich thuoc 1 byte
