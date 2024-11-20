b10048119ced64c35432ee06e61039a99bc86a89
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-11-20 16:00:44 +0700

    Upload bài tập session02-Ex05

---
+++ Session02-Ex05.c
@@ -0,0 +1,10 @@
+int main(void){
+	int dai = 5;
+	int rong = 3;
+	int C = 2*(dai+rong);
+	int S = dai*rong;
+	printf("Chu vi hcn la: %d\n", C);
+	printf("Dien tich hcn la: %d\n", S);
+	
+	return 0;
+} 
