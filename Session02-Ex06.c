27b1831a25c6e3440b418a2eefc4aac78d85ed19
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-11-21 07:43:53 +0700

    Update Session02-Ex06.c

--- Session02-Ex06.c
+++ Session02-Ex06.c
@@ -3,8 +3,8 @@ int main(void){
 	float r = 4;
 	float C = 2*r*pi;
 	float S = r*r*pi;
-	printf("Chu vi hinh tron la: %d\n", C);
-	printf("Dien tich hinh tron la: %d\n", S);
+	printf("Chu vi hinh tron la: %f\n", C);
+	printf("Dien tich hinh tron la: %f\n", S);
 	
 	return 0;
 }
